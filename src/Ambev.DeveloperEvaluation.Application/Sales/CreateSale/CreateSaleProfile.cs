62bb142d73d2f4da100509907e10b6282f19cebe
Gustavo Barroso <gustavo.barroso@bancobmg.com.br>
2025-02-10 11:51:34 -0300

    Reestruturando os profiles para o automapper

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleProfile.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Application.Shared.Commands;
+using Ambev.DeveloperEvaluation.Application.Shared.Results;
+using Ambev.DeveloperEvaluation.Domain.Entities.Sale;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
+{
+    public class CreateSaleProfile : Profile
+    {
+        public CreateSaleProfile()
+        {
+            CreateMap<UpsertSaleCommand, Sale>();
+            CreateMap<UpsertSaleProductCommand, SaleProduct>();
+
+
+            CreateMap<Sale, UpsertSaleResult>();
+            CreateMap<SaleProduct, UpsertSaleProductResult>();
+            CreateMap<SaleSubsidiary, UpsertSaleSubsidiaryResult>();
+            CreateMap<SaleCustomer, UpsertSaleCustomerResult>();
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleProfile.cs
@@ -0,0 +1,18 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Shared.Commands;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale
+{
+    public class CreateSaleProfile : Profile
+    {
+        public CreateSaleProfile()
+        {
+            CreateMap<CreateSaleRequest, CreateSaleCommand>();
+
+            CreateMap<CreateSaleCommand, UpsertSaleCommand>();
+            CreateMap<CreateSaleProductCommand, UpsertSaleProductCommand>();
+            CreateMap<CreateSaleProductsRequest, CreateSaleProductCommand>();
+        }
+    }
+}
